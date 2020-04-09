7fdccdd1ebdffb9e65d4c0f6de69a100c1a7af8d
Mirza <amirza.techlaunch.io>
2020-04-09 19:19:50 -0400

    scope and closure

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 //  1. Encapsulation : using encapsulation we group related variable variables and functions together. Encapsulation reduces complexity and increase reusability.
 
-// 2. Abtraction : we hide the details and complexity and shows only the essentials. Abtraction reduces complexity and increase reusability.
+// 2. Abstraction : we hide the details and complexity and shows only the essentials. Abstraction reduces complexity and increase reusability.
 
 // 3. Inheritance : Inheritance helps us eliminate redunded code. We can build an generic Object and help other Object inherite the Propertice and Method.
 
@@ -124,5 +124,73 @@ for (let key in circle){
 // if you want to get all the keys of an Object
 
 const keys = Object.keys(circle);
-console.log(keys);// it will return an array of keys .... in our example it will give => ["radius","draw"]
+console.log(keys);
+// it will return an array of keys .... in our example it will give => ["radius","draw"]   in this approch you cant seperate methods from properties
 
+// if you want to know if a property exits in an Object you can use 'in' operator
+
+if ('radius' in circle)
+    console.log("Circle has radius");
+
+// Abstraction : 
+
+// to hide certain members from outside we use the Abstraction.
+
+function CircleWithAbstraction(radius){
+
+    this.radius=radius;
+
+    // creating a local variable. the variable will die once the function execution ended
+
+    let defaultLocation = {x:0, y:0};
+
+    // creating a private methode... its not accessible from outside of the function 
+
+    let computeOptimumLocation = function(factor){
+        console.log(2*factor);
+    }
+
+    this.draw=function(){
+        computeOptimumLocation(0.1);
+        console.log( defaultLocation);
+    }
+
+    // write a methode to access the local variable
+    this.getDefaultLocation = function (){
+        return defaultLocation;
+    }
+
+    // define a local variable as a property of an Object and get it from outside of that object 
+
+    Object.defineProperty(this, 'defaultLocation',{
+        get: function(){
+            return defaultLocation;
+        },
+        //  set values to that property
+        set: function(value){
+            // validation of the value
+            if (!value.x || !value.y)
+                throw new Error('Invalid Location');
+            defaultLocation = value;
+
+        }
+    })
+};
+
+const circleWithAbstraction = new CircleWithAbstraction(10);
+
+// you can declare a local variable without setting that as a property of that object. In the example above,i have declared a local variable 'defaultLocation' using let keyword. I didn't set that as a property of that object using 'this'. So it's not going to be a part of 'CircleWithAbstraction' Object and you will not be able to access it from outside of the object. using this technik you can hide certain members from outside.
+
+// "scope" depends on execution. when we call a function , the local variable get out of scope once the function stop executing.
+
+// in contrast of "scope" we have "closure". A closure determines what variables will be accessible in an inner function. A function / method of an Object will be able to access all the variables that defined inside of that function as well as the variables defined in its parent function. The scope is temporary and die. Every time we call a function the local variable will be recreated and reinitialised and after that function it will die. 
+
+// As local variables are not accessible from outside and if you want to display the local variable somewhere in our website , we can simply create a methode and retuen that varible. By calling that methode we can easily access the variable. check "getDefaultLocation" methode.
+
+// using  'Object.defineProperty we can define the local variable as a property of an Object and get it from outside of that object like this
+
+circleWithAbstraction.defaultLocation;
+
+// we can also set the value of that property like this 
+
+circleWithAbstraction.defaultLocation = {x:1, y:1};
